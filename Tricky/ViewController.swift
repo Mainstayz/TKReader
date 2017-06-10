7233177030b7cf6e16a997f31994c8147624f5d3
hezongzhu <hezongzhu@gmail.com>
2017-06-11 00:07:13 +0800

    Initial Commit

---
+++ Tricky/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tricky
+//
+//  Created by Pillar on 2017/6/11.
+//  Copyright © 2017年 unkown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
